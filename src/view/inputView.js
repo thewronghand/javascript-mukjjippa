d72d74449829482ae28aad4ea326b30ee105a40a
thewronghand <penfreak77@gmail.com>
2022-12-05 22:41:57 +0900

    feat: inputView 내 입력 기능 구현

--- src/view/inputView.js
+++ src/view/inputView.js
@@ -1,3 +1,24 @@
-const inputView = {};
+const { Console } = require('@woowacourse/mission-utils');
+const { GAME_MESSAGE } = require('../utils/constants/message');
+
+const inputView = {
+  readRockPaperScissors(callback) {
+    Console.readLine(GAME_MESSAGE.readRockPaperScissors, () => {
+      callback(input);
+    });
+  },
+
+  readMukjjippa(callback) {
+    Console.readLine(GAME_MESSAGE.readMukjjippa, () => {
+      callback(input);
+    });
+  },
+
+  readGameCommand(callback) {
+    Console.readLine(GAME_MESSAGE.readGameCommand, () => {
+      callback(input);
+    });
+  }
+};
 
 module.exports = inputView;
