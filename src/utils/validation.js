4f1881981fd0f5cb74d7a27432e77a4de1e35a00
thewronghand <penfreak77@gmail.com>
2022-12-05 22:46:25 +0900

    feat: validate 객체 내 유효성 판별 하위 기능 구현

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,3 +1,27 @@
-const validate = {};
+const { VALUE, KEY } = require('./constants/value');
+
+const validate = {
+  rockPaperScissors(input) {
+    if (
+      input !== VALUE.rock &&
+      input !== VALUE.paper &&
+      input !== VALUE.scissors
+    ) {
+      throw new Error();
+    }
+  },
+
+  mukjjippa(input) {
+    if (input !== VALUE.muk && input !== VALUE.jji && input !== VALUE.ppa) {
+      throw new Error();
+    }
+  },
+
+  gameCommand(input) {
+    if (input !== KEY.restart && input !== KEY.quit) {
+      throw new Error();
+    }
+  }
+};
 
 module.exports = validate;
